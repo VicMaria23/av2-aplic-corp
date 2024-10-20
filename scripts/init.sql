1d02039f232e1f02e29601e9dd7e9c0ef8789199
VicMaria23 <vick4122@gmail.com>
2024-10-20 20:31:05 -0300

    adicionado swagger 3

---
+++ scripts/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS usuario (
+    cpf VARCHAR(11) PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    data_nasc DATE NOT NULL,
+    data_cad DATE NOT NULL,
+    ativo BOOLEAN NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
